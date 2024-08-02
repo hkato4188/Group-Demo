e0721982edb0549fbb318f56c55c5b54b390ad6b
Hiroki Kato <hirokikato1@gmail.com>
2024-08-02 15:17:19 -0400

    connected js files

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+console.log('it works');
+console.log("we know for sure");
+
+//saving <p></p>
+const paragraphJStest = document.getElementById("js-test");
+
+paragraphJStest.onclick = function () {
+    paragraphJStest.style.color = "purple";
+}
